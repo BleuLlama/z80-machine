6bcab50db7abb18c70cb061bc9e9ef765432fcda
Scott Lawrence <yorgle@gmail.com>
2016-07-07 12:07:38 -0400

    Attempt to throttle 6850 emulation

--- Common/src/mc6850_console.c
+++ Common/src/mc6850_console.c
@@ -28,6 +28,51 @@ void mc6850_out_console_control( byte data )
     /* do nothing -- ignored */
 }
 
+
+#ifdef THROTTLE_KBHIT
+
+#include <unistd.h>     // for usleep
+#include <sys/time.h>   // for timeval
+
+static long millis( void )
+{
+        static struct timeval startTime;
+
+        struct timeval endTime;
+        long seconds, useconds;
+        double duration;
+
+        if( startTime.tv_sec == 0 && startTime.tv_usec == 0 ) {
+                gettimeofday( &startTime, NULL );
+        }
+
+        gettimeofday( &endTime, NULL );
+        seconds = endTime.tv_sec - startTime.tv_sec;
+        useconds = endTime.tv_usec - startTime.tv_usec;
+
+        duration = seconds + useconds/1000000.0;
+
+        return( (long) (duration * 1000) );
+}
+
+int throttled_kbhit( void ) 
+{
+	static long nextm = 0;
+
+	if( z_kbhit() == 0 ) return 0;
+
+	if( millis() > nextm ) {
+		printf( "." );
+		nextm = millis() + 10;
+		return z_kbhit();
+	}
+
+	return 0;
+}
+#else /* THROTTLED KBHIT */
+#define throttled_kbhit		z_kbhit
+#endif /* THROTTLED KBHIT */
+
 /* read in data from the ACIA
  	if nothing available, returns 0xff (not sure if this is accurate)
 */
@@ -36,7 +81,7 @@ byte mc6850_in_console_data( void )
     byte val = 0xff;
 
     /* get in a byte from the console */
-    if( z_kbhit() ) {
+    if( throttled_kbhit() ) {
 	val = getchar();
     }
 
@@ -48,7 +93,7 @@ byte mc6850_in_console_status( void )
 {
     byte val = 0;
 
-    if( z_kbhit() ) {
+    if( throttled_kbhit() ) {
 	    val |= kPRS_RxDataReady; /* key is available to read */
     }
     val |= kPRS_TXDataEmpty;        /* we're always ready for new stuff */
