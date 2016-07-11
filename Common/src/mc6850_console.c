7e6c275442796f74b915e79e3241c0a336d916f2
Scott Lawrence <yorgle@gmail.com>
2016-07-11 17:17:16 -0400

    Throttled kbhit experimentation

--- Common/src/mc6850_console.c
+++ Common/src/mc6850_console.c
@@ -6,6 +6,18 @@
 #include <stdio.h>
 #include "mc6850_console.h"	/* port bit definitions */
 
+
+/* we have a throttled kbhit which reduces the number of MS between keypresses */
+#undef EXPERIMENTAL_THROTTLED_KBHIT
+
+/* time in milliseconds between keypresses */
+#define kThrottleMS	(1000)
+
+#ifdef EXPERIMENTAL_THROTTLED_KBHIT
+#include <unistd.h>     // for usleep
+#include <sys/time.h>   // for timeval
+#endif
+
 /* ********************************************************************** */
 
 /* initialize the ACIA */
@@ -29,49 +41,48 @@ void mc6850_out_console_control( byte data )
 }
 
 
-#ifdef THROTTLE_KBHIT
 
-#include <unistd.h>     // for usleep
-#include <sys/time.h>   // for timeval
 
-static long millis( void )
+#ifdef EXPERIMENTAL_THROTTLED_KBHIT
+
+static long long millis( void )
 {
-        static struct timeval startTime;
+    	struct timeval te; 
+	long long milliseconds;
 
-        struct timeval endTime;
-        long seconds, useconds;
-        double duration;
+	static long long startTime = 0;
 
-        if( startTime.tv_sec == 0 && startTime.tv_usec == 0 ) {
-                gettimeofday( &startTime, NULL );
-        }
+    	gettimeofday(&te, NULL); // get current time
+	if( startTime == 0 ) {
+		startTime = te.tv_sec*1000LL + te.tv_usec/1000; // caculate milliseconds
+	}
 
-        gettimeofday( &endTime, NULL );
-        seconds = endTime.tv_sec - startTime.tv_sec;
-        useconds = endTime.tv_usec - startTime.tv_usec;
-
-        duration = seconds + useconds/1000000.0;
-
-        return( (long) (duration * 1000) );
+	milliseconds = te.tv_sec*1000LL + te.tv_usec/1000; // caculate milliseconds
+	//printf("milliseconds: %lld\n", milliseconds);
+	return milliseconds;
 }
 
 int throttled_kbhit( void ) 
 {
-	static long nextm = 0;
+	static long long nextm = 0;
 
-	if( z_kbhit() == 0 ) return 0;
+	if( !z_kbhit() ) return 0;
 
 	if( millis() > nextm ) {
-		printf( "." );
-		nextm = millis() + 10;
+		nextm = millis() + kThrottleMS;
 		return z_kbhit();
 	}
 
 	return 0;
 }
-#else /* THROTTLED KBHIT */
-#define throttled_kbhit		z_kbhit
-#endif /* THROTTLED KBHIT */
+
+#define KBHIT	throttled_kbhit
+
+#else 
+
+#define KBHIT	z_kbhit
+
+#endif /* EXPERIMENTAL_THROTTLED_KBHIT */
 
 /* read in data from the ACIA
  	if nothing available, returns 0xff (not sure if this is accurate)
@@ -81,8 +92,8 @@ byte mc6850_in_console_data( void )
     byte val = 0xff;
 
     /* get in a byte from the console */
-    if( throttled_kbhit() ) {
-	val = getchar();
+    if( z_kbhit() ) {
+	    val = getchar();
     }
 
     return val;
@@ -93,7 +104,7 @@ byte mc6850_in_console_status( void )
 {
     byte val = 0;
 
-    if( throttled_kbhit() ) {
+    if( KBHIT() ) {
 	    val |= kPRS_RxDataReady; /* key is available to read */
     }
     val |= kPRS_TXDataEmpty;        /* we're always ready for new stuff */
